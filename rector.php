4422e9de34baaa128a781f71859fc09c0e22c22c
Yuri Oliveira <yusadeol@gmail.com>
2025-03-14 18:49:17 -0300

    feat: initial setup

---
+++ rector.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/bin',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withSkip([
+        __DIR__.'/bootstrap/cache',
+    ])
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true
+    )
+    ->withPhpSets();
